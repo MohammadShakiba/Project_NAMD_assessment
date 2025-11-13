2f4470cbf1fac8bbf169cadcc46e33765b2374a0
MohammadShakiba <mshakiba.kerman.iran@gmail.com>
2025-11-13 11:36:04 -0500

    Updated repository of NAMD assessment project

--- 1_NAMD/run_all_ssy.sh
+++ 1_NAMD/run_all_ssy.sh
@@ -1,74 +1,68 @@
 #!/bin/bash
-ntraj=2000
-nstep=105000
+ntraj=1000
+dt=5.0
+elec_int=2
+nstep=420000
+rep=1
+istate=1
 # For no decoherence, BCSH, and ID-A
 for tshmethod in fssh fssh2 gfsh; do 
   for mqcmethod in 1 2 3 ; do
-    sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --ntraj $ntraj --nsteps $nstep --dt 20.0 --ssy True /g" submit_template.slm
-    echo "python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --ntraj $ntraj --nsteps $nstep --dt 20.0 /g" submit_template.slm
+    sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --ntraj $ntraj --nsteps $nstep --dt $dt --istate $istate --rep $rep --elec_int $elec_int --ssy True  /g" submit_template.slm
+    echo "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --ntraj $ntraj --nsteps $nstep --dt $dt --istate $istate --rep $rep --elec_int $elec_int --ssy True  /g" 
     sbatch submit_template.slm 
   done
 done
-
-# For SHXF with different wave packet width scale
+# For SHXF with different wave packet width scale, mqcmethod = 4
 for tshmethod in fssh fssh2 gfsh; do
-  #for wpwidth in 0.05 0.1 0.25 0.5 1.0 2.0 4.0 ; do
-  for wpwidth in 0.0001 0.001 0.005 ; do
-    sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod 4 --ntraj $ntraj --nsteps $nstep --dt 20.0 --wpwidth $wpwidth --ssy True /g" submit_template.slm
-    echo "python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod 4 --ntraj $ntraj --nsteps $nstep --dt 20.0 --wpwidth $wpwidth /g" submit_template.slm
+  for wpwidth in 0.05 0.1 0.25 0.5 1.0 2.0 4.0 ; do
+    sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod 4 --ntraj $ntraj --nsteps $nstep --dt $dt --wpwidth $wpwidth --istate $istate --rep $rep --elec_int $elec_int --ssy True  /g" submit_template.slm
+    echo "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod 4 --ntraj $ntraj --nsteps $nstep --dt $dt --wpwidth $wpwidth --istate $istate --rep $rep --elec_int $elec_int --ssy True  /g"
     sbatch submit_template.slm
   done
 done
- 
- # For SDM, MFSD, and DISH_REV23 with EDC decoherence time
- for tshmethod in fssh fssh2 gfsh; do
-   for mqcmethod in 5 6 7 ; do
-     for eps in 0.01 0.05 0.1 0.2 0.4 1.0 5.0 10.0 20.0 40.0 80.0; do
-       sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime edc --ntraj $ntraj --nsteps $nstep --dt 20.0 --eps $eps --ssy True /g" submit_template.slm
-       echo "python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime edc --ntraj $ntraj --nsteps $nstep --dt 20.0 --eps $eps /g" submit_template.slm
-       sbatch submit_template.slm
-     done 
-   done
- done
- 
- # For SDM, MFSD, and DISH_REV23 with Schwartz 1 decoherence time
- for tshmethod in fssh fssh2 gfsh; do
-   for mqcmethod in 5 6 7 ; do
-     for A in 0.0001 0.001 0.01 0.1 1.0 10.0 ; do
-       sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime schw1 --ntraj $ntraj --nsteps $nstep --dt 20.0 --A $A --ssy True/g" submit_template.slm
-       echo "python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime schw1 --ntraj $ntraj --nsteps $nstep --dt 20.0 --A $A /g" submit_template.slm
-       sbatch submit_template.slm
-     done
-   done
- done
-
-
-# For SDM, MFSD, and DISH_REV23 with Schwartz 2 decoherence time
-  for tshmethod in fssh fssh2 gfsh; do
-    for mqcmethod in 5 6 7 ; do
-      for A in 0.0001 0.001 ; do
-      #for A in 0.01 0.1 1.0 10.0 20.0; do
-        sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime schw2 --ntraj $ntraj --nsteps $nstep --dt 20.0 --A $A --ssy True/g" submit_template.slm
-        echo "python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime schw2 --ntraj $ntraj --nsteps $nstep --dt 20.0 --A $A /g" submit_template.slm
-        sbatch submit_template.slm
-      done
+# For SDM and DISH_REV23 with EDC decoherence time, mqcmethod = 5 or 6
+for tshmethod in fssh fssh2 gfsh; do
+  for mqcmethod in 5 6 ; do
+    for eps in 0.01 0.05 0.1 0.2 0.4 1.0 5.0 10.0 50.0 100.0 500.0 1000.0; do
+      sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime edc --ntraj $ntraj --nsteps $nstep --dt $dt --eps $eps --istate $istate --rep $rep --elec_int $elec_int --ssy True   /g" submit_template.slm
+      echo "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime edc --ntraj $ntraj --nsteps $nstep --dt $dt --eps $eps --istate $istate --rep $rep --elec_int $elec_int --ssy True   /g"
+      sbatch submit_template.slm
+    done 
+  done
+done
+# For DISH_REV23 with Schwartz 1 decoherence time, mqcmethod = 6
+for tshmethod in fssh fssh2 gfsh; do
+  for mqcmethod in 6 ; do
+    for A in 0.0001 0.001 0.01 0.1 1.0 10.0 100.0 1000.0 10000.0 100000.0 1000000.0 10000000.0 100000000.0 1000000000.0; do
+      sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime schw1 --ntraj $ntraj --nsteps $nstep --dt $dt --A $A --istate $istate --rep $rep --elec_int $elec_int --ssy True  /g" submit_template.slm
+      echo "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime schw1 --ntraj $ntraj --nsteps $nstep --dt $dt --A $A --istate $istate --rep $rep --elec_int $elec_int --ssy True  /g"
+      sbatch submit_template.slm
     done
   done
+done
 
-# For SDM, MFSD, and DISH_REV23 with Gu-Franco decoherence time
+# For SDM and DISH_REV23 with Schwartz 2 decoherence time, mqcmethod = 5 or 6
 for tshmethod in fssh fssh2 gfsh; do
-  for mqcmethod in 5 6 7 ; do
-    sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime gu_franco --ntraj $ntraj --nsteps $nstep --dt 20.0 --ssy True/g" submit_template.slm
-    echo "python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime gu_franco --ntraj $ntraj --nsteps $nstep --dt 20.0 /g" submit_template.slm
+  for mqcmethod in 5 6 ; do
+    for A in 0.0001 0.001 0.01 0.1 1.0 10.0 100.0 1000.0 10000.0 100000.0 1000000.0 10000000.0 100000000.0 1000000000.0; do
+      sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime schw2 --ntraj $ntraj --nsteps $nstep --dt $dt --A $A --istate $istate --rep $rep --elec_int $elec_int --ssy True  /g" submit_template.slm
+      echo "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime schw2 --ntraj $ntraj --nsteps $nstep --dt $dt --A $A --istate $istate --rep $rep --elec_int $elec_int --ssy True  /g"
       sbatch submit_template.slm
+    done
   done
 done
 
-# For the new decoherence algorithm proposed by Alexey with Gu-Franco decoherence time 
+# For SDM and DISH_REV23 with Gu-Franco decoherence time
 for tshmethod in fssh fssh2 gfsh; do
-  sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod 8 --dectime gu_franco --ntraj $ntraj --nsteps $nstep --dt 20.0 --ssy True/g" submit_template.slm
-  echo "python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod 8 --dectime gu_franco --ntraj $ntraj --nsteps $nstep --dt 20.0 /g" submit_template.slm
-  sbatch submit_template.slm
+  for mqcmethod in 5 6 ; do
+    for reorg_energy in 0.000000000125 0.0000125 0.000125 0.0125 1.25 12.5 125.0 1250.0 ; do
+      sed -i "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime gu_franco --ntraj $ntraj --nsteps $nstep --dt $dt --istate $istate --rep $rep --elec_int $elec_int --ssy True --reorg_energy $reorg_energy /g" submit_template.slm
+      echo "s/python.*/python spin_boson_2_states.py --iter 0 --tshmethod $tshmethod --mqcmethod $mqcmethod --dectime gu_franco --ntraj $ntraj --nsteps $nstep --dt $dt --istate $istate --rep $rep --elec_int $elec_int --ssy True --reorg_energy $reorg_energy /g" 
+      sbatch submit_template.slm
+    done
+  done
 done
 
 
+   
--- 1_NAMD/spin_boson_2_states.py
+++ 1_NAMD/spin_boson_2_states.py
@@ -16,20 +16,21 @@ import models
 import libra_py.dynamics.tsh.compute as tsh_dynamics
 import libra_py.dynamics.tsh.plot as tsh_dynamics_plot
 import libra_py.data_savers as data_savers
-
-from recipes import fssh, fssh2, fssh3, gfsh, ida, sdm, mfsd, shxf, bcsh, sdm_schw1, sdm_schw2, shxf_edc, shxf_schw1, shxf_schw2, mfsd_edc, mfsd_schw2, dish_rev2023_edc, dish_rev2023_schw1, dish_rev2023_schw2
-
 import libra_py.models.GLVC as GLVC
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--iter', default=0, type=int)
 parser.add_argument('--ntraj', default=10, type=int)
+parser.add_argument('--istate', default=1, type=int)
+parser.add_argument('--rep', default=1, type=int)
+parser.add_argument('--elec_int', default=2, type=int)
 parser.add_argument('--mqcmethod', default=1, type=int)
 parser.add_argument('--tshmethod', default='fssh', type=str)
 parser.add_argument('--dectime', default='', type=str)
 parser.add_argument('--nsteps', default=100000, type=int)
 parser.add_argument('--dt', default=1.0, type=float)
 parser.add_argument('--temperature', default=300.0, type=float)
+parser.add_argument('--reorg_energy', default=0.0125, type=float)
 parser.add_argument('--A', default=10.0, type=float)
 parser.add_argument('--eps', default=0.1, type=float)
 parser.add_argument('--ssy', default=False, type=bool)
@@ -54,13 +55,18 @@ model_params = {"model": 0, "model0": 0,"nstates": 2, "num_osc": 10, "coupling_s
 ##### 2. Choose the Nonadiabatic Dynamics Methodology 
 ############################################################
 NSTATES = 2
-A = MATRIX(2,2); A.set(0, 0, args.A); A.set(1,1, args.A)
+A = MATRIX(10,1); 
+for i in range(10):
+    A.set(i, 0, 1/args.A) #; A.set(1,1, args.A)
+# Initialize the parameters in dyn_general, these parameters will
+# be updated for each mqc method below
 dyn_general = { "nsteps":args.nsteps, "ntraj":args.ntraj, "nstates":NSTATES,
                 "dt": args.dt, "num_electronic_substeps":1, "isNBRA":0, "is_nbra":0,
                 "progress_frequency":0.1, "which_adi_states":range(NSTATES), "which_dia_states":range(NSTATES),      
-                "mem_output_level":3,
-                #"properties_to_save":[ "timestep", "time","se_pop_adi", "se_pop_dia", "sh_pop_adi", "sh_pop_dia"],
-                "properties_to_save":[ "time", "sh_pop_adi", "Epot_ave", "Ekin_ave", "Etot_ave" ],  
+                "mem_output_level":4,
+                #"properties_to_save":[ "timestep", "time","se_pop_adi", "se_pop_dia", "sh_pop_adi", "sh_pop_dia",
+                #                       "ave_decoherence_rates" ],  
+                "properties_to_save":[ "time", "sh_pop_adi", "sh_pop_dia", "se_pop_adi", "se_pop_dia"],
                 "prefix":"", "prefix2":"",
                 "ham_update_method":1, "ham_transform_method":1, "time_overlap_method":1, 
                 "nac_update_method":1, "hvib_update_method":1, "force_method":1, "rep_force":1,
@@ -69,17 +75,24 @@ dyn_general = { "nsteps":args.nsteps, "ntraj":args.ntraj, "nstates":NSTATES,
                 "decoherence_times_type":-1, "dephasing_informed":0, 
                 "decoherence_rates":MATRIX(2,2), 
                 "ave_gaps":MATRIX(2,2), 
-                "do_ssy":0, "rep_tdse":1, "electronic_integrator":2, 
+                "do_ssy":0, "rep_tdse":args.rep, "electronic_integrator":args.elec_int, 
                 "state_tracking_algo":-1, "do_phase_correction":1, 
                 "A_val_schw": args.A, "eps_val_edc": args.eps,
-                #"wp_width":0.3, "coherence_threshold": 0.01,
-                #"project_out_aux": 1, 
                 "schwartz_decoherence_inv_alpha": A,
                 "decoherence_C_param": 1.0, "decoherence_eps_param":0.1
               }
 # The assessing method
+# The MQC method:
+#   1: no decoherence
+#   2: BCSH
+#   3: ID-A
+#   4: SHXF -> with external parameters of: wp_width, coherence_threshold, project_out_aux
+#   5: SDM  -> with external parameters of: decoherence_times_type, decoherence_C_param, decoherence_eps_param, schwartz_decoherence_inv_alpha
+#   6: DISH  -> with external parameters of: decoherence_times_type, decoherence_C_param, decoherence_eps_param, schwartz_decoherence_inv_alpha
 mqcmethod = args.mqcmethod
+# The TSH mehod: fssh, fssh2, gfsh
 tshmethod = args.tshmethod
+# Decohrence time method: schw1, schw2, and edc
 dectime = args.dectime
 # =========== No decoherence
 if mqcmethod==1:
@@ -103,33 +116,14 @@ elif mqcmethod==5:
     if dectime=='edc':
         folder_prefix = 'SDM_EDC'
         dyn_general.update( { "decoherence_times_type":1, "decoherence_C_param": 1.0, "decoherence_eps_param": args.eps } )  # EDC + default params
-    elif dectime=='schw1':
-        folder_prefix = 'SDM_SCHW1'
-        dyn_general.update( { "decoherence_times_type":2, "schwartz_decoherence_inv_alpha":A } ) # Schwartz version 1
     elif dectime=='schw2':
         folder_prefix = 'SDM_SCHW2'
         dyn_general.update( { "decoherence_times_type":3, "schwartz_decoherence_inv_alpha":A } ) # Schwartz version 2
     elif dectime=='gu_franco':
         folder_prefix = 'SDM_Gu_Franco' 
-        dyn_general.update({"Temperature": args.temperature, "reorg_energy": 0.0125, "decoherence_times_type": 4 })
-# ============ MFSD 
-elif mqcmethod==6:
-    dyn_general.update({"tsh_method":-1 }) # adiabatic, no surface hopping
-    dyn_general.update({ "decoherence_algo":4}) # mfsd
-    if dectime=='edc':
-        folder_prefix = 'MFSD_EDC'
-        dyn_general.update( { "decoherence_times_type":1, "decoherence_C_param": 1.0, "decoherence_eps_param": args.eps } )  # EDC + default params
-    elif dectime=='schw1':
-        folder_prefix = 'MFSD_SCHW1'
-        dyn_general.update( { "decoherence_times_type":2, "schwartz_decoherence_inv_alpha":A } ) # Schwartz version 1 - only this option is possible for MFSD!
-    elif dectime=='schw2':
-        folder_prefix = 'MFSD_SCHW2'
-        dyn_general.update( { "decoherence_times_type":3, "schwartz_decoherence_inv_alpha":A } ) # Schwartz version 2
-    elif dectime=='gu_franco':
-        folder_prefix = 'MFSD_Gu_Franco' 
-        dyn_general.update({"Temperature": args.temperature, "reorg_energy": 0.0125, "decoherence_times_type": 4 })
+        dyn_general.update({"Temperature": args.temperature, "reorg_energy": args.reorg_energy, "decoherence_times_type": 5 })
 # ============ DISH_REV23
-elif mqcmethod==7:
+elif mqcmethod==6:
     dyn_general.update({ "decoherence_algo":7}) # new DISH
     if dectime=='edc':
         folder_prefix = 'DISH_REV23_EDC'
@@ -142,11 +136,7 @@ elif mqcmethod==7:
         dyn_general.update( { "decoherence_times_type":3, "schwartz_decoherence_inv_alpha":A } ) # Schwartz version 2
     elif dectime=='gu_franco':
         folder_prefix = 'DISH_REV23_Gu_Franco'
-        dyn_general.update({"Temperature": args.temperature, "reorg_energy": 0.0125, "decoherence_times_type": 4 })
-# ============ New_decoherence_method with Gu-Franco
-elif mqcmethod==8:
-    folder_prefix = 'new_decoherence_with_Gu_Franco'
-    dyn_general.update({"Temperature": args.temperature, "reorg_energy": 0.0125, "decoherence_algo":9, "decoherence_times_type":4 })
+        dyn_general.update({"Temperature": args.temperature, "reorg_energy": args.reorg_energy, "decoherence_times_type": 5 })
 #=========== Phase correction of SSY =================
 if args.ssy:
     dyn_general.update({"do_ssy": 1}) 
@@ -186,22 +176,24 @@ nucl_params = { "ndof":ndof,
                 # Average of the position for each dof
                 "q": q_init,
                 # Average of the momentum for each dof
-                "p":[0.0 for i in range(ndof)],
+                "p": [0.0 for i in range(ndof)],
                 "mass":list(model_params["mass"]),
                 "force_constant":[ model_params["mass"][i]*model_params["omega"][0][i]**2 for i in range(_nosc) ],
-                "q_width":[ 1.0/( np.sqrt(model_params["mass"][i] * beta) * model_params["omega"][0][i]) for i in range(_nosc)  ],
-                "p_width":[ 1.0/( np.sqrt(beta/model_params["mass"][i])) for i in range(_nosc)  ],
+                "q_width":[ 1.0/( np.sqrt(model_params["mass"][i] * beta) * model_params["omega"][0][i]) for i in range(_nosc)],
+                #"q_width":[ 1.0/np.sqrt(2*model_params["mass"][i] * model_params["omega"][0][i]) for i in range(_nosc)],
+                "p_width":[ 1.0/( np.sqrt(beta/model_params["mass"][i])) for i in range(_nosc)],
+                #"p_width":[ np.sqrt(model_params["mass"][i] * model_params["omega"][0][i] / 2 ) for i in range(_nosc)],
                 "init_type": 4
               }
 # Electronic initial conditions - start on the second diabatic state (starting from 0 = ground)
 ##########
-istate = 1
+istate = args.istate
 ##########
 istates = [0.0 for i in range(NSTATES)]
 istates[istate] = 1.0     
 print(istates)
 elec_params = {"verbosity":2, "init_dm_type":0,"ndia":NSTATES, "nadi":NSTATES, 
-               "rep":1, "init_type":0, "istates":istates, "istate":istate     }
+               "rep": args.rep, "init_type":0, "istates":istates, "istate":istate     }
 
 
 def compute_model(q, params, full_id):
@@ -250,10 +242,12 @@ elif 'schw' in dectime:
 elif mqcmethod==4:
     pref = pref + F'_wpwidth_scale_{args.wpwidth}'
 elif mqcmethod==8 or 'gu_franco' in dectime:
-    pref = pref + F'_temperature_{args.temperature}' 
+    pref = pref + F'_temperature_{args.temperature}_reorg_energy_{args.reorg_energy}'
 
 if args.ssy:
     pref = pref + '_SSY'
+
+pref = pref + f'_istate_{args.istate}_rep_{args.rep}_elec_int_{args.elec_int}'
 
 dyn_params.update({ "prefix":pref, "prefix2":pref })
 dyn_general.update({ "prefix":pref, "prefix2":pref })
